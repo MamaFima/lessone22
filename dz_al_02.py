2bdb4ee0f20b53aee9de9d746462ae9f545c85ae
MamaFima <164761211+MamaFima@users.noreply.github.com>
2024-10-18 15:08:48 +0700

    Create dz_al_02.py

---
+++ dz_al_02.py
@@ -0,0 +1,31 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        else:
+            return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
+stack = Stack()
+stack.push(10)
+stack.push(20)
+stack.push(30)
+print(stack.pop())  # 30
+print(stack.peek())  # 20
